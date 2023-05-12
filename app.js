c1f2e0382957ce4d3203b180629e43c441237fab
Mauricio Arrieta <mauricio.desarrollador@gmail.com>
2023-05-12 13:50:19 -0500

    Soluciono error al cerrar el modal de apps

--- app.js
+++ app.js
@@ -2,15 +2,13 @@
     const $menu = d.querySelector('.menu');
     const $app = d.querySelector('.apps')
 
-    $menu.addEventListener('click', (e)  => {
+    $menu.addEventListener('click', (e)=> {
+        e.stopPropagation()
         $app.classList.toggle('none');
     })
 
-    // d.addEventListener('click', (e) => {
-    //     // Elije a cualquier elemento a que sea hijo de .menu
-    //     if(!e.target.matches('html')) return false;
-    //     $app.classList.add('none');     
-    // })
-
+    d.addEventListener('click', ()=> {
+        $app.classList.add('none');
+    })
 
 })(document)
